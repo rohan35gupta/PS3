3c7556f72b0168b7683d909dac72d12b461b9485
Rohan Gupta <rohangupta@wustl.edu>
2018-02-13 01:35:20 -0600

    1. Define new class: door

---
+++ problemset3.R
@@ -0,0 +1,12 @@
+#Rohan Gupta
+#February 13, 2018
+#Pol Sci 4626 Problem Set 3
+
+#1.
+one<-1 #Objects of class take on one numeric value: 1,
+two<-2 #2,
+three<-3 #or 3--indicating which door candidate chooses
+class(one)<-"door" #Defined new class: door
+class(two)<-"door"
+class(three)<-"door"
+
