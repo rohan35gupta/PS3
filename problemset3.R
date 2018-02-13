cb4cfdb9240801f5b5595f8a066430e476eebf42
Rohan Gupta <rohangupta@wustl.edu>
2018-02-13 02:18:10 -0600

    4. Did it again, this time using S4 class system

--- problemset3.R
+++ problemset3.R
@@ -11,9 +11,11 @@ class(two)<-"door"
 class(three)<-"door"
 
 #2.
-PlayGame<-function(x){ #Created method for door objects called PlayGame
+PlayGame<-function(x){
   UseMethod("PlayGame",x)
 }
+#Created method for door objects called PlayGame
+
 PlayGame.door<-function(x){ #Method takes numeric value stored in door object
   random<-sample(1:3,1) #Method draws random number between 1 and 3 that presents door behind which car is hidden
   if(x==random){ #Method compares the two numbers
@@ -25,3 +27,36 @@ PlayGame.door<-function(x){ #Method takes numeric value stored in door object
 }
 
 #4.
+setClass(Class="door",
+         representation=representation(
+           x="numeric"
+         ),
+         prototype=prototype(
+           x=c()
+         )
+         )
+#Wrote construction function that allows user to create door object
+
+setValidity("door",function(object){
+  if(object@x%%1!=0){
+    return("@x is not an integer")
+  }
+}
+)
+#Wrote validation function that checks whether value stored in door is actually an integer
+
+setGeneric("PlayGame",
+           function(object="door"){
+             standardGeneric("PlayGame")
+           })
+setMethod("PlayGame","door",
+          function(object){
+            random<-sample(1:3,1)
+            if(object@x==random){
+              print("Congratulations! You win. You chose the correct door.")
+            }
+            else{
+              print("Sorry! You lose. You chose the wrong door.")
+            }
+          })
+#Wrote new generic method PlayGame explained above
