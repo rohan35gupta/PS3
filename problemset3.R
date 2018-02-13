37d7d27ba63e81c6cbe9a3a7c87f930e89851fee
Rohan Gupta <rohangupta@wustl.edu>
2018-02-13 01:49:19 -0600

    3. Did 1 & 2 using S3 class system

--- problemset3.R
+++ problemset3.R
@@ -10,3 +10,18 @@ class(one)<-"door" #Defined new class: door
 class(two)<-"door"
 class(three)<-"door"
 
+#2.
+PlayGame<-function(x){ #Created method for door objects called PlayGame
+  UseMethod("PlayGame",x)
+}
+PlayGame.door<-function(x){ #Method takes numeric value stored in door object
+  random<-sample(1:3,1) #Method draws random number between 1 and 3 that presents door behind which car is hidden
+  if(x==random){ #Method compares the two numbers
+    print("Congratulations! You win. You chose the correct door.") #Method prints message congratulating winning candidate that chose correct door
+  }
+  else{
+    print("Sorry! You lose. You chose the wrong door.") #Method prints message expressing sympathies for losing candidate that chose wrong door
+  }
+}
+
+#4.
